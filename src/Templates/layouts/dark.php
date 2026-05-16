5a6ebf5fb0595b3ce933bee97c51a6fee36fef25
Juanan Rodriguez <juananrodriguezmartinez@gmail.com>
2026-05-16 11:08:21 +0200

    Body: Grid -> Flexbox (flex-direction:column, main flex:1)

--- src/Templates/layouts/dark.php
+++ src/Templates/layouts/dark.php
@@ -4,13 +4,13 @@
 <head>
     <?= $this->renderPartial('head', ['meta' => $meta, 'jsDataScript' => $jsDataScript]) ?>
 </head>
-<body class="theme-dark" style="display:grid;grid-template-rows:auto 1fr auto;min-height:100dvh;margin:0;">
+<body class="theme-dark" style="display:flex;flex-direction:column;min-height:100dvh;margin:0;">
     <?= $this->renderPartial('header', [
         'escape' => $escape,
         'isAuthenticated' => isset($_SESSION['mn_user']),
     ]) ?>
 
-    <main class="pt-20 pb-8" style="max-width:724px;width:100%;margin:0 auto;">
+    <main class="pt-20 pb-8 flex-1" style="max-width:724px;width:100%;margin:0 auto;">
         <?= $content ?>
     </main>
 
